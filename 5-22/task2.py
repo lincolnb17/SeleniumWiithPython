5ac7dd206e3f07b1977555ab38469b4fa9c47515
Lincolm <Lincoln>
2022-05-22 22:45:13 +0545

    LoginLogout

--- 5-22/task2.py
+++ 5-22/task2.py
@@ -5,13 +5,14 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 username = 'student'
 password = 'Password123'
-
+#sample test for login and logout
 def SingupLogoutTest(username,password):
     driver.get("https://practicetestautomation.com/practice-test-login/")
     driver.find_element_by_xpath("//input[@id='username']").send_keys(username)
     driver.find_element_by_xpath("//input[@id='password']").send_keys(password)
     driver.find_element_by_xpath("//button[@id='submit']").click()
     driver.find_element_by_xpath("//a[normalize-space()='Log out']").click()
+
     
 SingupLogoutTest(username,password)
     
